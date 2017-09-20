569ab105a947fced6319c986265423f2a3f54ae6
ibrobabs <babskolawole@gmail.com>
2017-09-20 00:24:14 -0600

    update

--- home/models.py
+++ home/models.py
@@ -9,19 +9,28 @@ class HomeCarousel(models.Model):
     description = models.TextField(max_length=3000, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return str(self.title)
 
 class HomeIntro(models.Model):
     title = models.CharField(max_length=300, null=True, blank=True)
     intro = models.TextField(max_length=None)
 
     def __str__(self):
-        return self.title
+        return str(self.title)
+
+class Reminder(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    button = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.title)
 
 class Marketing(models.Model):
     image   = models.ImageField(upload_to='images/')
     title   = models.CharField(max_length=200, null=True, blank=True)
     description  = models.TextField(max_length=3000, null=True, blank=True) 
+    read_more = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
-        return self.title 
+        return str(self.title) 
--- home/views.py
+++ home/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
 
-from .models import HomeCarousel, Marketing
+from .models import HomeCarousel, Marketing, HomeIntro
 
 def home(request):
     carousel = HomeCarousel.objects.all()
     market = Marketing.objects.all()
+    intro = HomeIntro.objects.all()
     context = {
         "carousel": carousel,
-        "market": market
+        "market": market,
+        "intro": intro
     }
     template = "home/home.html"
     return render(request, template, context)

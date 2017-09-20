eaa55707204f70b77528133cd47be0df7600d710
ibrobabs <babskolawole@gmail.com>
2017-09-19 23:33:08 -0600

    update

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import HomeCarousel, Marketing
+from .models import HomeCarousel, Marketing, HomeIntro
 
 admin.site.register(HomeCarousel)
 admin.site.register(Marketing)
+admin.site.register(HomeIntro)
--- home/models.py
+++ home/models.py
@@ -11,6 +11,13 @@ class HomeCarousel(models.Model):
     def __str__(self):
         return self.title
 
+class HomeIntro(models.Model):
+    title = models.CharField(max_length=300, null=True, blank=True)
+    intro = models.TextField(max_length=None)
+
+    def __str__(self):
+        return self.title
+
 class Marketing(models.Model):
     image   = models.ImageField(upload_to='images/')
     title   = models.CharField(max_length=200, null=True, blank=True)

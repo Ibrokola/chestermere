fc6a046189318c052308ba3cddbdd53380a01837
ibrobabs <babskolawole@gmail.com>
2017-09-22 03:11:55 -0600

    update

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+import time
 
 
 from .models import HomeCarousel, Marketing, HomeIntro, Reminder
 
 def home(request):
     carousel = HomeCarousel.objects.all()
+    localtime = time.asctime(time.localtime(time.time()))
     market = Marketing.objects.all().order_by('?')
     intro = HomeIntro.objects.all()
     remind = Reminder.objects.all().order_by('?')
     context = {
+        "localtime": localtime,
         "carousel": carousel,
         "market": market,
         "intro": intro,

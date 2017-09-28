2caa58ec554592cbfb4652a0f560d6a2bd867d6e
ibrobabs <babskolawole@gmail.com>
2017-09-27 23:04:38 -0600

    update

--- chestermere/urls.py
+++ chestermere/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^about/', include('about.urls')),
     url(r'^forms/', include('registry.urls', namespace='reg')),
     url(r'^calendar/', include('additional.urls')),
+    url(r'^search/', include('search.urls', namespace='search')),
     url(r'^services/', include('services.urls', namespace='services')),
     url(r'^certificates/', include('certificates.urls', namespace='cert')),
 ]

0a6f2a682ff1c0a17e25596f2a783debd7e91287
ibrobabs <babskolawole@gmail.com>
2017-10-01 19:28:45 -0600

    update

--- chestermere/urls.py
+++ chestermere/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,12 +10,15 @@ from home.views import home
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name='home'),
+    url(r'^terms/$', TemplateView.as_view(template_name="terms.html")),
+    url(r'^privacy/$', TemplateView.as_view(template_name="privacy.html")),
     url(r'^about/', include('about.urls')),
     url(r'^forms/', include('registry.urls', namespace='reg')),
     url(r'^renewal/', include('additional.urls')),
     url(r'^search/', include('search.urls', namespace='search')),
     url(r'^services/', include('services.urls', namespace='services')),
     url(r'^certificates/', include('certificates.urls', namespace='cert')),
+
 ]
 
 

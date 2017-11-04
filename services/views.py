21666a4dcae38314393e47db4a133056f5aaf092
ibrobabs <babskolawole@gmail.com>
2017-11-04 17:27:16 -0600

    update

--- services/views.py
+++ services/views.py
@@ -13,25 +13,37 @@ from .models import ImageHeader, Service
 
 def service_list(request):
     index_page = ImageHeader.objects.all()
-    service1 = Service.objects.filter(parts__title='part1').order_by('-updated')
-    service2 = Service.objects.filter(parts__title='part2').order_by('-updated')
-    service3 = Service.objects.filter(parts__title='part3').order_by('-updated')
+    service1 = Service.objects.filter(parts__title='part1').order_by('timestamp')
+    service2 = Service.objects.filter(parts__title='part2').order_by('timestamp')
+    service3 = Service.objects.filter(parts__title='part3').order_by('timestamp')
+    service4 = Service.objects.filter(parts__title='part4').order_by('timestamp')
+    service5 = Service.objects.filter(parts__title='part5').order_by('timestamp')
+    service6 = Service.objects.filter(parts__title='part6').order_by('timestamp')
+    service7 = Service.objects.filter(parts__title='part7').order_by('timestamp')
     # service = Service.objects.all()
     template = 'services/list_view.html'
     context ={
         "index_page": index_page,
         "service3": service3,
         "service1": service1,
-        "service2": service2
+        "service2": service2,
+        "service4": service4,
+        "service5": service5,
+        "service6": service6,
+        "service7": service7,
     }
     return render(request, template, context)
 
 
 def service_detail(request, slug=None):
     # service1 = Service.objects.all()
-    service1 = Service.objects.filter(parts__title='part1').order_by('-updated')
-    service2 = Service.objects.filter(parts__title='part2').order_by('-updated')
-    service3 = Service.objects.filter(parts__title='part3').order_by('-updated')
+    service1 = Service.objects.filter(parts__title='part1').order_by('timestamp')
+    service2 = Service.objects.filter(parts__title='part2').order_by('timestamp')
+    service3 = Service.objects.filter(parts__title='part3').order_by('timestamp')
+    service4 = Service.objects.filter(parts__title='part4').order_by('timestamp')
+    service5 = Service.objects.filter(parts__title='part5').order_by('timestamp')
+    service6 = Service.objects.filter(parts__title='part6').order_by('timestamp')
+    service7 = Service.objects.filter(parts__title='part7').order_by('timestamp')
     try:
         service = get_object_or_404(Service, slug=slug)
     except Service.MultipleObjectsReturned:
@@ -42,6 +54,10 @@ def service_detail(request, slug=None):
         "service1": service1,
         "service2": service2,
         "service3": service3,
-        "service": service
+        "service": service,
+        "service4": service4,
+        "service5": service5,
+        "service6": service6,
+        "service7": service7,
     }
     return render(request, template, context)
